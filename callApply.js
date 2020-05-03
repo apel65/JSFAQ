74648846575a76666b13cb17b14d298793b96ab2
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-05-03 10:46:34 +0800

    again practice with bind

--- callApply.js
+++ callApply.js
@@ -7,11 +7,32 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     getCharge: function(amount){
+        console.log(this)
         this.salery -= amount;
         return this.salery;
     }
 }
 
-normalPerson.getCharge(150);
-normalPerson.getCharge(3000);
-console.log(normalPerson.salery);
+const friendlyPerson = {
+    firstName: 'Hero',
+    lastName: 'Golam',
+    salery: 25000
+}
+
+const heroAlom = {
+    firstName: 'Hero',
+    lastName: 'Alom',
+    salery: 10000
+}
+
+// const friendlyPersonChargeBill = normalPerson.getCharge.bind(friendlyPerson);
+// friendlyPersonChargeBill(2000);
+// console.log(friendlyPerson)
+// console.log(normalPerson.salery)
+
+const heroAlomChargeBill = normalPerson.getCharge.bind(heroAlom);
+heroAlomChargeBill(1000);
+console.log(heroAlom);
+
+normalPerson.getCharge(200);
+console.log(normalPerson)
