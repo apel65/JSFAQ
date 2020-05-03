04c1bda3314809ff213f935f94e86349162f2dfc
ApelMahmud65 <apelmahmud.kpi@gmail.com>
2020-05-04 07:52:25 +0800

    learn about of async and await

--- arguments.js
+++ arguments.js
@@ -1,15 +1,15 @@
-//arguments is a array like object
+// //arguments is a array like object
 
-function addNum(a, b){
-    var sum = 0;
-    for (let i = 0; i < arguments.length; i++) {
-        const element = arguments[i];
-        sum += element;
-    }
+// function addNum(a, b){
+//     var sum = 0;
+//     for (let i = 0; i < arguments.length; i++) {
+//         const element = arguments[i];
+//         sum += element;
+//     }
     
-    return sum;
-}
+//     return sum;
+// }
 
-var result = addNum(3, 5, 5, 3, 6, 3, 6);
+// var result = addNum(3, 5, 5, 3, 6, 3, 6);
 
-console.log(result);
+// console.log(result);
---
+++ new.js
@@ -0,0 +1,13 @@
+
+class HeroPerson  {
+    constructor(firstName, lastName, salery){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salery = salery;
+    }
+}
+
+const helalUddin = new HeroPerson('Helal', 'Uddin', 5000);
+const sefuga = new HeroPerson('Sefuga', 'Begum', 4500);
+console.log(sefuga);
+console.log(helalUddin)
